bf5518f0c0cc098459926fc301c1c9d4a474353a
Michael Carroll <ineffable@gmail.com>
2021-05-09 19:28:48 +1000

    update translations and build

--- buildzip.sh
+++ buildzip.sh
@@ -5,9 +5,11 @@ intltool-extract --type=gettext/glade Settings.ui
 intltool-extract --type=gettext/glade Settings4.ui
 xgettext -k -k_ -kN_ -o locale/BingWallpaper.pot Settings.ui.h Settings4.ui.h extension.js prefs.js blur.js utils.js convenience.js --from-code=UTF-8
 
+echo "Translation status" > translations.txt
 for D in locale/*; do
     if [ -d "${D}" ]; then
-        msgfmt -o "${D}/LC_MESSAGES/BingWallpaper.mo" "${D}/LC_MESSAGES/BingWallpaper.po"   # your processing here
+        msgfmt --template=BingWallpaper.pot --statistics --verbose -o "${D}/LC_MESSAGES/BingWallpaper.mo" "${D}/LC_MESSAGES/BingWallpaper.po"  2>> translations.txt 
+        # your processing here
     fi
 done
 
