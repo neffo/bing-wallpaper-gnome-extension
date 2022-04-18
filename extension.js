82344e848f8830ced852b8531342991dcb97ee9a
Michael Carroll <ineffable@gmail.com>
2022-04-18 21:03:24 +1000

    scale thumbnails on hidpi displays

--- extension.js
+++ extension.js
@@ -374,6 +374,7 @@ class BingWallpaperIndicator extends PanelMenu.Button {
     // set menu thumbnail
     _setThumbnailImage() {
         let pixbuf = this.thumbnail.pixbuf;
+        let scale_factor = 1;
         if (pixbuf == null)
             return;
         const {width, height} = pixbuf;
@@ -395,7 +396,9 @@ class BingWallpaperIndicator extends PanelMenu.Button {
         getActorCompat(this.thumbnailItem).hexpand = false;
         getActorCompat(this.thumbnailItem).vexpand = false;
         getActorCompat(this.thumbnailItem).content = image;
-        getActorCompat(this.thumbnailItem).set_size(480, 270);    
+        scale_factor = St.ThemeContext.get_for_stage(global.stage).scale_factor;
+        log('scale factor: ' + scale_factor);
+        getActorCompat(this.thumbnailItem).set_size(480*scale_factor, 270*scale_factor);
         this.thumbnailItem.setSensitive(true);
     }
 
