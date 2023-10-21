4488c69a7d35253b299d9e3ea3e4a53f36a57feb
Michael Carroll <ineffable@gmail.com>
2023-10-21 17:53:00 +1000

    support thumbnail scaling

--- thumbnail.js
+++ thumbnail.js
@@ -9,13 +9,18 @@
 import Gio from 'gi://Gio';
 import GdkPixbuf from 'gi://GdkPixbuf';
 
+const THUMBNAIL_WIDTH = 480;
+const THUMBNAIL_HEIGHT = 270;
+
 export default class Thumbnail {
-    constructor(filePath) {
+    constructor(filePath, scale = 1.0) {
         if (!filePath) {
             throw new Error(`need argument ${filePath}`);
         }
         try {
-            this.pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(filePath, 480, 270);
+            let w = Math.round(THUMBNAIL_WIDTH * scale);
+            let h = Math.round(THUMBNAIL_HEIGHT * scale);
+            this.pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(filePath, w, h);
             this.srcFile = Gio.File.new_for_path(filePath);
         } catch (err) {
             log('Unable to create thumbnail for corrupt or incomplete file: ' + filePath + ' err: ' + err);
